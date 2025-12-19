44a672ac1a791725d62dbe25c4f3accc588ed271
MohammedAli-Biar <maab16@alu.ua.es>
2025-12-19 11:17:50 +0100

    wooo
    liada de gpt

--- dataB.js
+++ dataB.js
@@ -1,12 +1,9 @@
 require("dotenv").config();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const MONGOPATH = process.env.MONGO_URI || 'mongodb://localhost:27017/predict';
 
-mongoose.connect(MONGOPATH, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(MONGOPATH)
   .then(() => console.log('Conectado a la base de datos.'))
   .catch(err => console.error('Error conectando a la base de datos:', err));
 
